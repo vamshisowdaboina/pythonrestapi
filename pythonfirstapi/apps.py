bc44c39ca97d07257e4f917fd3185cd175e1f2b7
vamshi vms <vamshi.sowdaboina@gmail.com>
2020-01-18 14:59:29 -0600

    added django

---
+++ pythonfirstapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PythonfirstapiConfig(AppConfig):
+    name = 'pythonfirstapi'
---
+++ pythonrestapi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pythonrestapi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonrestapi.settings')
+
+application = get_wsgi_application()
