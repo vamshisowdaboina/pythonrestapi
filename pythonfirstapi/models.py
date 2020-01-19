3544dbff8a490a9907443b66aaaa6c4411f3e1e4
vamshi vms <vamshi.sowdaboina@gmail.com>
2020-01-18 19:54:33 -0600

    models updated

--- pythonfirstapi/models.py
+++ pythonfirstapi/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import *
 
-# Create your models here.
+class UserProfile(AbstractBaseUser permissionMixin):
+    """docstring"""
+   email = models.EmailField(max_length=225 unique=True)
+   name = models.CharField(max_length=225)
+   is_active = models.BooleanField(default=True)
+   is_staff = models.BooleanField(default=True)
+
+   objects = UserProfileMnager()
+
+   USERNAME.FIELD = 'email'
+   REQUIRE.FIELDS = ['name']
+
+   def get_full_name(self):
+       """retrive fullname"""
+       return self.name
+
+    def get_short_name(self):
+        """shorname"""
+        return self.name
+
+    def _str_(self):
+        """return string representation"""
+        return self.email
