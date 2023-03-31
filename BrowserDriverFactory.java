ed4dc9eb430f498248c0c8a20078af4839652461
vamshisowdaboina <42748387+vamshisowdaboina@users.noreply.github.com>
2023-03-31 17:38:25 -0500

    Create BrowserDriverFactory.java

---
+++ BrowserDriverFactory.java
@@ -0,0 +1,27 @@
+package com.ZAP_Selenium_BrowserDriver;
+
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BrowserDriverFactory {
+
+	// Make reference variable for WebDriver
+	static WebDriver driver;
+
+	public static WebDriver createChromeDriver(Proxy proxy, String path) {
+		// Set proxy in the chrome browser
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability("proxy", proxy);
+		
+		// Set system property for chrome driver with the path
+		System.setProperty("webdriver.chrome.driver", path);
+		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		ChromeOptions options = new ChromeOptions();
+		options.merge(capabilities);
+		return new ChromeDriver(options);
+	}
+}
